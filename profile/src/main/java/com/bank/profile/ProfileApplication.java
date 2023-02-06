d0c80024d89f0c71fe1d0bc4a4c55056e91fc47b
Дмитрий Теницкий <lodroft@gmail.com>
2023-02-06 16:32:44 +0000

    Подключение Eureka в profile [bk-9]

--- profile/src/main/java/com/bank/profile/ProfileApplication.java
+++ profile/src/main/java/com/bank/profile/ProfileApplication.java
@@ -2,7 +2,9 @@ package com.bank.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProfileApplication {
     public static void main(String[] args) {
