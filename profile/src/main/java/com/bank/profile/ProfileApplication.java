f99deb94ce9d4f95dc4fca6e3af1cc5e5afc364e
Дмитрий Теницкий <lodroft@gmail.com>
2023-02-10 16:26:19 +0000

    Подключение рест хендлера в profile [bk-25]

--- profile/src/main/java/com/bank/profile/ProfileApplication.java
+++ profile/src/main/java/com/bank/profile/ProfileApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bank.common", "com.bank.profile"})
 public class ProfileApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProfileApplication.class, args);
