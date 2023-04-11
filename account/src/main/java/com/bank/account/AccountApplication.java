f63004776e2a9e3a0e95ff866d3725f45daf1d95
Ilia Ponomarev <i.a.ponomarev.work@gmail.com>
2023-04-11 19:12:48 +0000

    Покрыл аннотациями swagger контроллер в account bk-42

--- account/src/main/java/com/bank/account/AccountApplication.java
+++ account/src/main/java/com/bank/account/AccountApplication.java
@@ -1,11 +1,16 @@
 package com.bank.account;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"com.bank.common", "com.bank.account"})
+@OpenAPIDefinition(info = @Info(title = "Информация о банковских счетах",
+        description = "API для получения информации о банковских счетах",
+        version = "v1"))
 public class AccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
