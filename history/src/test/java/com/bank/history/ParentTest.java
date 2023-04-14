e97b3f482341042b1527bb11ce453cd4cdce2098
Константин Полунин <tinyyy052@gmail.com>
2023-04-14 18:27:26 +0000

    Покрыл аннотациями Swagger контроллер history [bk-45]

---
+++ history/src/test/java/com/bank/history/ParentTest.java
@@ -0,0 +1,16 @@
+package com.bank.history;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class ParentTest {
+
+    protected static final Long ONE = 1L;
+    protected static final Long TWO = 2L;
+    protected static final Long THREE = 3L;
+
+}
--- history/src/test/java/com/bank/history/mapper/HistoryMapperTest.java
+++ history/src/test/java/com/bank/history/mapper/HistoryMapperTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.bank.history.supplier.HistorySupplier.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
